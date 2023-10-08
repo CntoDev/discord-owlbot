cf84ad53b523af8305f6f0a2c99db29390b8e0b7
Enrico Ghidoni <enricoghdn@gmail.com>
2023-10-08 19:51:17 +0200

    Existing code integration

---
+++ op-reminder.sh
@@ -0,0 +1,3 @@
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate discord-owlbot
+python /home/carpenoctem/discord-owlbot/op-start-reminder.py
---
+++ op-start-reminder.py
@@ -0,0 +1,24 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CHANNEL_ID = 1138803439690846211
+BOT_SECRET = os.environ['OWLBOT_SECRET']
+
+class MyClient(discord.Client):
+     async def on_ready(self):
+        # async for guild in client.fetch_guilds(limit=150):
+        #         print(guild.name)
+
+        channel = self.get_channel(CHANNEL_ID)
+        await channel.send("Tonight's OP is about to start, @here grab a drink and join us!")
+        await self.close()
+
+intents = discord.Intents()
+intents.messages = True
+intents.guilds = True
+
+client = MyClient(intents=intents)
+client.run(BOT_SECRET)
